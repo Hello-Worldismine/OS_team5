cf09ca774e0c6daff2f481420b59743bebda6f32
sayyyho <323psh@naver.com>
2025-06-04 23:48:46 +0900

    chore

--- OS_team5/source/cat.h
+++
@@ -1,9 +0,0 @@
-#include "../include/main.h"
-
-#ifndef CAT_H
-#define CAT_H
-
-int concatenate(DirectoryTree* dirTree, char* fName, int type);
-int ft_cat(DirectoryTree* dirTree, char* cmd);
-
-#endif // CAT_H
